90c4c58d97432183c3587054d844d1b62ac6c6b2
Ahmet Yasin Öztop <ahmetyasinoztop81@gmail.com>
2024-04-14 13:42:45 +0300

    react version update

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import Projects from "./components/Projects";
 import Services from "./components/Services";
 import Sidebar from "./components/Sidebar";
 import ScrollToTop from "./components/ScrollToTop";
+import { Analytics } from "@vercel/analytics/react";
 import { SpeedInsights } from "@vercel/speed-insights/react";
 
 function App() {
@@ -52,6 +53,7 @@ function App() {
         <Contact />
         <Footer />
         <ScrollToTop />
+        <Analytics />
         <SpeedInsights />
       </header>
     </div>
